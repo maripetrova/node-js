3430fac6b53e3e59f58aa4fd037a5eb2cbaac9ed
maripetrova <masha.petrova.27012004@gmail.com>
2021-09-29 17:35:16 +0300

    Правка от 29.09.21

--- 29.09.21/script.js
+++ 29.09.21/script.js
@@ -1,9 +1,16 @@
 const http = require("http");
-const port = 3001
+const port = 3001;
+const fs = require("fs");
+
+
 const server = http.createServer((rec, res) => {
-    res.write("<img src=\'original.jpeg\'>");
+    // res.write("<img src=\"original.jpeg\" type=\"image/jpeg\">");
+    let img = fs.readFileSync("./original.jpeg");
+    console.log(img);
+    // res.write("AAA");
+    res.write(img)
     res.end();
 })
 server.listen(port, err => {
-    console.log(`server starting ${port}`)
+    console.log(`server starting ${port}`);
 });
